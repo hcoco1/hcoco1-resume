bc24a60abc3ae55aaf94053b5c1d4132d0e0f523
Ivan <arias.ivan@gmail.com>
2023-11-09 07:30:28 -0400

    defineConfig added

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,6 +5,6 @@ import tailwind from "@astrojs/tailwind";
 
 // https://astro.build/config
 export default defineConfig({
-  site: 'https://astrofy-template.netlify.app',
+  site: 'https://resume.hcoco1.com/',
   integrations: [mdx(), sitemap(), tailwind()]
 });
