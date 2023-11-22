f4366d1d37693fe66d31c4a96cfee9b42a84ec05
Ivan <arias.ivan@gmail.com>
2023-11-21 23:46:06 -0500

    phone added

--- src/config.ts
+++ src/config.ts
@@ -1,5 +1,5 @@
 // Place any global data in this file.
 // You can import this data from anywhere in your site by using the `import` keyword.
 
-export const SITE_TITLE = 'Ivan | Resume';
-export const SITE_DESCRIPTION = 'Ivan Arias | Full Stack Development with Flask and React | Geologist |';
+export const SITE_TITLE = 'Ivan Arias';
+export const SITE_DESCRIPTION = 'Full Stack Development with Flask and React | Geologist';
