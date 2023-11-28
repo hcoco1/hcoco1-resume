c24f8230a4e71d1d6e86c715098e3c63dc95151f
Ivan <arias.ivan@gmail.com>
2023-11-28 14:13:58 -0500

    astro config updated

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,10 +3,12 @@ import mdx from '@astrojs/mdx';
 import sitemap from '@astrojs/sitemap';
 import tailwind from "@astrojs/tailwind";
 
+import react from "@astrojs/react";
+
 // https://astro.build/config
 export default defineConfig({
-  site: 'https://hcoco1.com/',
-  integrations: [mdx(), sitemap(), tailwind()],
+  site: 'https://www.hcoco1.com/',
+  integrations: [mdx(), sitemap(), tailwind(), react()],
   socialMedia: {
     image: '/public/profile.jpg',
     description: 'Ivan Arias| Full Stack Development with Flask and React | Geologist'
