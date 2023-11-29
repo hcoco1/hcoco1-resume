ad244ced3201ebb83c708391b53a6dcd7a4af2dc
Ivan <arias.ivan@gmail.com>
2023-11-28 20:53:41 -0500

    react icons deleted

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,15 +2,20 @@ import { defineConfig } from 'astro/config';
 import mdx from '@astrojs/mdx';
 import sitemap from '@astrojs/sitemap';
 import tailwind from "@astrojs/tailwind";
+import yaml from '@rollup/plugin-yaml';
 
 import react from "@astrojs/react";
 
 // https://astro.build/config
 export default defineConfig({
+  vite: {
+    plugins: [yaml()]
+  },
   site: 'https://www.hcoco1.com/',
   integrations: [mdx(), sitemap(), tailwind(), react()],
   socialMedia: {
     image: '/public/profile.jpg',
     description: 'Ivan Arias| Full Stack Development with Flask and React | Geologist'
   }
+  
 });
